50264f7e3cc0cf454d104e45b7ab2d3e5c147a8a
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2026-03-05 13:06:17 +0300

    Update creditcard_fraud(plan).py

--- Welcome_to_PyerualJetwork/ExampleCodes/creditcard_fraud(plan).py
+++ Welcome_to_PyerualJetwork/ExampleCodes/creditcard_fraud(plan).py
@@ -8,16 +8,18 @@ x = data.drop('Class', axis=1).values
 y = data['Class'].values
 
 x = data_ops.normalization(x)
+
 x_train, x_test, y_train, y_test = data_ops.split(x, y, 0.4, 42)
-
 y_train, y_test = data_ops.encode_one_hot(y_train, y_test)
-
-scaler_params, x_train, x_test = data_ops.standard_scaler(x_train, x_test)
-
 x_train, y_train = data_ops.synthetic_augmentation(x_train, y_train)
-
 # Configuring optimizer
-genetic_optimizer = lambda *args, **kwargs: ene.evolver(*args, **kwargs)
-model = nn.learn(x_train, y_train, genetic_optimizer, fit_start=True, batch_size=0.1, auto_normalization=False, gen=50, pop_size=100)
 
+genetic_optimizer = lambda *args, **kwargs: ene.evolver(*args, **kwargs)
+gradient_optimizer = lambda *args, **kwargs: nn.grad(*args, **kwargs)
+
+model = nn.learn(x_train, y_train, gradient_optimizer=gradient_optimizer, genetic_optimizer=genetic_optimizer, fit_start=True, quick_start=True, neurons=[64], activation_functions=['relu'], backprop_train=True, iter=[1,39], pop_size=100)
 test_model = nn.evaluate(x_test, y_test, model, show_report=True)
+
+print('Test ACC:', test_model[model_ops.get_acc()])
+
+model_ops.save_model(model, 'credit_card_ptnn')
