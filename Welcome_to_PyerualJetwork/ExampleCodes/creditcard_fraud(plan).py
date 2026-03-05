e10166114898861f1631ce203a328fd4ea3cff45
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2026-03-05 13:09:07 +0300

    Update creditcard_fraud(plan).py

--- Welcome_to_PyerualJetwork/ExampleCodes/creditcard_fraud(plan).py
+++ Welcome_to_PyerualJetwork/ExampleCodes/creditcard_fraud(plan).py
@@ -15,11 +15,12 @@ x_train, y_train = data_ops.synthetic_augmentation(x_train, y_train)
 # Configuring optimizer
 
 genetic_optimizer = lambda *args, **kwargs: ene.evolver(*args, **kwargs)
-gradient_optimizer = lambda *args, **kwargs: nn.grad(*args, **kwargs)
+gradient_optimizer = lambda *args, **kwargs: nn.grad(*args, method='adam', **kwargs)
 
-model = nn.learn(x_train, y_train, gradient_optimizer=gradient_optimizer, genetic_optimizer=genetic_optimizer, fit_start=True, quick_start=True, neurons=[64], activation_functions=['relu'], backprop_train=True, iter=[1,39], pop_size=100)
+model = nn.learn(x_train, y_train, gradient_optimizer=gradient_optimizer, genetic_optimizer=genetic_optimizer, fit_start=True, quick_start=True, neurons=[64], activation_functions=['relu'], loss='categorical_corssentropy', backprop_train=True, iter=[1,39], pop_size=100)
 test_model = nn.evaluate(x_test, y_test, model, show_report=True)
 
 print('Test ACC:', test_model[model_ops.get_acc()])
 
 model_ops.save_model(model, 'credit_card_ptnn')
+
