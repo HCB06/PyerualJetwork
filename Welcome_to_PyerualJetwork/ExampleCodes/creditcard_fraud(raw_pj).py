25ac942ab41933e719491b015cb6d42d44d630be
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2026-03-07 23:29:26 +0300

    Update creditcard_fraud(raw_pj).py

--- Welcome_to_PyerualJetwork/ExampleCodes/creditcard_fraud(raw_pj).py
+++ Welcome_to_PyerualJetwork/ExampleCodes/creditcard_fraud(raw_pj).py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pyerualjetwork.cpu import data_ops
-from pyerualjetwork import nn, ene, model_ops
+from pyerualjetwork import nn, model_ops
 
 data = pd.read_csv('creditcard.csv') # dataset link: https://www.kaggle.com/datasets/mlg-ulb/creditcardfraud
 
@@ -16,11 +16,12 @@ x_train, y_train = data_ops.synthetic_augmentation(x_train, y_train)
 
 gradient_optimizer = lambda *args, **kwargs: nn.grad(*args, method='adam', **kwargs)
 
-model = nn.learn(x_train, y_train, gradient_optimizer=gradient_optimizer, fit_start=False, neurons=[64], activation_functions=['relu'], loss='categorical_corssentropy', backprop_train=True, iter=40)
+model = nn.learn(x_train, y_train, gradient_optimizer=gradient_optimizer, fit_start=False, neurons=[64], activation_functions=['relu'], loss='categorical_corssentropy', backprop_train=True, iter=40, pop_size=0)
 test_model = nn.evaluate(x_test, y_test, model, show_report=True)
 
 print('Test ACC:', test_model[model_ops.get_acc()])
 
 model_ops.save_model(model, 'credit_card_ptnn')
+
 
 
